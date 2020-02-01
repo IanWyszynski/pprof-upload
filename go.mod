f6494c9c987aa4c944c2b994ebc390dcfd713688
Jan Wyszynski <wyszynski@google.com>
2020-01-31 16:50:58 -0800

    poll latest profile changes and update and online stackdriver profile

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.46.3
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/golang/protobuf v1.3.2
 	github.com/google/pprof v0.0.0-20190930153522-6ce02741cba3
 	google.golang.org/api v0.11.0
 	google.golang.org/genproto v0.0.0-20191009194640-548a555dbc03
--- main.go
+++ main.go
@@ -21,11 +21,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/url"
 	"os"
 	"time"
 
 	"cloud.google.com/go/compute/metadata"
+	"github.com/fsnotify/fsnotify"
+	"github.com/golang/protobuf/ptypes/duration"
 	"github.com/google/pprof/profile"
 	"google.golang.org/api/option"
 	gtransport "google.golang.org/api/transport/grpc"
@@ -40,8 +41,11 @@ var (
 	target  string
 	version string
 
-	input    string
-	keepTime bool
+	input        string
+	keepTime     bool
+	durationSecs int
+
+	latestProfile *pb.Profile
 )
 
 const (
@@ -57,6 +61,7 @@ func main() {
 	flag.StringVar(&version, "version", "", "")
 	flag.StringVar(&input, "i", "pprof.out", "")
 	flag.BoolVar(&keepTime, "keep-time", false, "")
+	flag.IntVar(&durationSecs, "duration", 15, "")
 	flag.Usage = usageAndExit
 	flag.Parse()
 
@@ -87,49 +92,96 @@ func main() {
 	}
 	client = pb.NewProfilerServiceClient(conn)
 
-	pprofBytes, err := ioutil.ReadFile(input)
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatalf("Cannot convert perf data to pprof: %v", err)
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+	done := make(chan bool)
+
+	payload, err := ioutil.ReadFile(input)
+	if err != nil {
+		log.Fatalf("unable to read file %v: %v\n", input, err)
+		return
+	}
+	latestProfile, err = create(ctx, payload)
+	if err != nil {
+		log.Fatalf("unable to create profile (does it already exist?): %v\n", err)
+		return
 	}
 
-	if err := upload(ctx, pprofBytes); err != nil {
-		log.Fatalf("Cannot upload to Stackdriver Profiler: %v", err)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if (event.Op&fsnotify.Create != 0) || (event.Op&fsnotify.Write != 0) {
+					payload, err := ioutil.ReadFile(input)
+					if err != nil {
+						log.Fatalf("unable to read file %v: %v\n", input, err)
+						continue
+					}
+					latestProfile, err = create(ctx, payload)
+					if err != nil {
+						log.Fatalf("unable to update profile: %v\n", err)
+						continue
+					}
+					_, err = update(ctx, payload)
+					if err != nil {
+						log.Fatalf("unable to update profile: %v\n", err)
+						continue
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Fatalf("error watching %v with fsnotify: %v\n", input, err)
+			}
+		}
+	}()
+
+	if err := watcher.Add(input); err != nil {
+		log.Fatalf("unable to set up fsnotify watcher for file %v: %v\n", input, err)
+		return
 	}
-	fmt.Printf("https://console.cloud.google.com/profiler/%s;type=%s?project=%s\n", url.PathEscape(target), pb.ProfileType_CPU, project)
+
+	<-done
+
+	log.Printf("shutting down pprof-upload agent\n")
+
+	if err := watcher.Close(); err != nil {
+		log.Fatalf("unable to set up fsnotify watcher for file %v: %v\n", input, err)
+		return
+	}
 }
 
-func upload(ctx context.Context, payload []byte) error {
-	// Reset time, otherwise old profiles wont be shown
-	// at Cloud profiler due to data retention limits.
-	if !keepTime {
-		var err error
-		payload, err = resetTime(payload)
-		if err != nil {
-			log.Printf("Cannot reset the profile's time: %v", err)
-		}
-	}
-
-	req := &pb.CreateOfflineProfileRequest{
-		Parent: "projects/" + project,
-		Profile: &pb.Profile{
-			// TODO(jbd): Guess the profile type from the input.
-			ProfileType: pb.ProfileType_CPU,
-			Deployment: &pb.Deployment{
-				ProjectId: project,
-				Target:    target,
-				Labels: map[string]string{
-					"zone":    zone,
-					"version": version,
-				},
-			},
-			ProfileBytes: payload,
+func create(ctx context.Context, payload []byte) (*pb.Profile, error) {
+	deployment := &pb.Deployment{
+		ProjectId: project,
+		Target:    target,
+		Labels: map[string]string{
+			"zone":    zone,
+			"version": version,
 		},
 	}
+	req := &pb.CreateProfileRequest{
+		Parent:      "projects/" + project,
+		Deployment:  deployment,
+		ProfileType: []pb.ProfileType{pb.ProfileType_CPU},
+	}
+	return client.CreateProfile(ctx, req)
+}
 
-	// TODO(jbd): Is there a way without having
-	// to load the profile all in memory?
-	_, err := client.CreateOfflineProfile(ctx, req)
-	return err
+func update(ctx context.Context, payload []byte) (*pb.Profile, error) {
+	newProfile := &pb.Profile{
+		Name:         latestProfile.Name,
+		ProfileType:  latestProfile.ProfileType,
+		Deployment:   latestProfile.Deployment,
+		Duration:     &duration.Duration{Seconds: int64(durationSecs), Nanos: 0},
+		ProfileBytes: payload,
+		Labels:       latestProfile.Labels,
+	}
+	req := &pb.UpdateProfileRequest{
+		Profile: newProfile,
+	}
+	return client.UpdateProfile(ctx, req)
 }
 
 // TODO(jbd): Make it optional.
@@ -161,6 +213,7 @@ Other options:
 -keep-time  When set, keeps the original time info from the profile file.
 			Due to data retention limits, Stackdriver Profiler won't
             show data older than 30 days. By default, false.
+-duration   Set the duration (in seconds) that each profile accounts for.
 `
 
 func usageAndExit() {
